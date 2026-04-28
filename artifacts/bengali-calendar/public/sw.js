ba8c0bc6b265c8d6b105a0205f393be8d7802e67
atnusingha9-debug <atnusingha9@gmail.com>
2026-04-28 16:58:48 +0530

    Update sw.js

--- artifacts/bengali-calendar/public/sw.js
+++ artifacts/bengali-calendar/public/sw.js
@@ -5,15 +5,3 @@ self.options = {
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
 
-
-
-
-
-
-
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10936957
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
